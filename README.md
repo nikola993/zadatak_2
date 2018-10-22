ab3e19a47976a2ae45f2e680a3365aa56d865f6a
nikola993 <cira93rxrx@hotmail.com>
2018-10-22 21:59:41 +0200

    zadatak2

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-```
-yarn install
-```
+pokreni npm install za instalaciju svih potrebnih paketa,
+zadatak se porece preko node index.js ili npm start posle pokretanja
+ovih komandi je potrebno uneti broj jedinica koje se bore
+zatim ce se u konzoli ispisivati tok borbe kako je trazeno u zadatku
--- src/fight.js
+++ src/fight.js
@@ -1,6 +1,6 @@
-class Units {
-    constructor() {
-        this.name = 0;
+class Unit {
+    constructor(name) {
+        this.name = name;
         this.health = 100;
         this.rechargeTime = (1000 * this.health / 100);
         this.damage = (this.health / 100);
@@ -11,68 +11,64 @@ class Units {
     findTarget(units) {
         let target;
         const numberOfUnits = units.length;
-        const reamningUnits = units.filter(unit => unit.health > 1); // kada jedinica ima manje od 1 health-a izbacuje se iz niza
-        if (reamningUnits.length >= 2) { // dok postoje vise od 2 jedinice trazi metu
-            do {
-                target = Math.floor(Math.random() * numberOfUnits);
-            }
-            while (target === this.name || units[target].health <= 1);
-            return target;
+        do {
+            target = Math.floor(Math.random() * numberOfUnits);
         }
-        if (this.health > 1) { // provera koja od zadnje dve jedinice ima vise od 1 health
-            console.log(`jedinica[${this.name}] je pobedila!!!!`);
-            process.exit();
-        }
-        return -1;
+        while (units[target].health < 0);
+        return units[target];
     }
 
     // Metoda za napad
-    async attack(units) {
+    attack(units) {
         const target = this.findTarget(units);
-        if (target !== -1 && this.health > 1) {
-            await this.doDamage(units, target);
-            console.log(`jedinica[${this.name}] -> jedinica[${target}]; jedinica[${target}].health = ${units[target].health}`);
-            if (units[target].health < 1) {
-                console.log(`jedinica[${units[target].name}] je mrtva;`);
-            }
-            await this.readyToAttack(units); // poziv metode za odbrojavanje do sledeceg napada
-        }
-    }
-
-    // Metoda za regulisanje statova posle napada
-    doDamage(units, target) {
-        units[target].health -= this.criticalStrike();
-        units[target].rechargeTime = (1000 * this.health / 100);
-        units[target].damage = (this.health / 100);
-        units[target].criticalChance = (10 - this.health / 10);
-    }
-
-    // Metoda za racunanje da li je napad critical strike
-    criticalStrike() {
-        if (this.criticalChance >= Math.floor(Math.random() * 100)) {
-            return this.damage * 2;
-        }
-        return this.damage;
-    }
-
-    // Metoda za racunanje vremena do sledeceg napada
-    readyToAttack(units) {
-        return new Promise(() => {
+        const reamningUnits = units.filter(unit => unit.health > 0); // kada jedinica ima manje od 1 health-a izbacuje se iz niza
+        if (this.health > 0) {
+            target.receiveDamage(this.doDamage());
+            console.log(`jedinica[${this.name}] -> jedinica[${target.name}]; jedinica[${target.name}].health = ${target.health}`);
             setTimeout(() => {
                 this.attack(units);
             }, this.rechargeTime);
-        });
+        } else {
+            console.log(`jedinica[${this.name}] je mrtva;`);
+            if (reamningUnits.length === 1) {
+                console.log(`jedinica[${target.name}] je pobedila!!!`);
+                process.exit();
+            }
+        }
+    }
+
+    doDamage() {
+        return this.damage;
+    }
+
+    // Metoda za regulisanje statova posle napada
+    receiveDamage(damage) {
+        this.health -= this.criticalStrike(damage);
+        this.rechargeTime = (1000 * this.health / 100);
+        this.damage = (this.health / 100);
+        this.criticalChance = (10 - this.health / 10);
+    }
+
+    // Metoda za racunanje da li je napad critical strike
+    criticalStrike(damage) {
+        if (this.criticalChance >= Math.floor(Math.random() * 100)) {
+            return damage * 2;
+        }
+        return damage;
     }
 }
 
 // kreiranje jedinica
 function makeUnits(numberOfUnit) {
-    const unit = new Array(numberOfUnit);
+    const units = [];
     for (let i = 0; i < numberOfUnit; i += 1) {
-        unit[i] = new Units();
-        unit[i].name = i;
+        units.push(new Unit(i));
     }
-    return unit;
+    return units;
+}
+
+function filterUnits(units, me) {
+    return units.filter(unit => unit.name !== me);
 }
 
 const standardInput = process.stdin;
@@ -81,7 +77,8 @@ standardInput.on('data', (data) => { // Unos broja jedinica
     if (data <= 5 && data > 1 && data !== '\n\n') {
         const units = makeUnits(data);
         units.forEach((unit) => { // pokretanje napada svih jedinica
-            unit.attack(units);
+            const otherUnits = filterUnits(units, unit.name);
+            unit.attack(otherUnits);
         });
     } else {
         console.log('Morate uneti broj manji od 6 a veci od 1');
